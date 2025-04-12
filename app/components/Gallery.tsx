7917bfae7604f283f3066fc3ff01cec2ec95ec97
Ushnish Ghosal <ghosalushnish@gmail.com>
2025-04-12 23:19:57 +0530

    hjg

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -3,22 +3,22 @@
 import { useEffect, useRef } from "react";
 
 const images = [
-  "/public/Events/2.jpeg",
-  "/public/Events/3.jpeg",
-  "/public/Events/4.jpeg",
-  "/public/Events/5.jpeg",
-  "/public/Events/6.jpeg",
-  "/public/Events/7.jpeg",
-  "/public/Events/8.jpg",
-  "/public/Events/9.jpg",
-  "/public/Events/10.jpg",
-  "/public/Events/11.jpg",
-  "/public/Events/14.jpeg",
-  "/public/Events/21.jpg",
-  "/public/Events/22.jpg",
-  "/public/public/public/Events/23.jpg",
-  "/public/Events/24.jpeg",
-  "/public/Events/25.jpg",
+  "/Events/2.jpeg",
+  "/Events/3.jpeg",
+  "/Events/4.jpeg",
+  "/Events/5.jpeg",
+  "/Events/6.jpeg",
+  "/Events/7.jpeg",
+  "/Events/8.jpg",
+  "/Events/9.jpg",
+  "/Events/10.jpg",
+  "/Events/11.jpg",
+  "/Events/14.jpeg",
+  "/Events/21.jpg",
+  "/Events/22.jpg",
+  "/Events/23.jpg",
+  "/Events/24.jpeg",
+  "/Events/25.jpg",
 ];
 
 const Gallery = () => {
@@ -44,7 +44,7 @@ const Gallery = () => {
   return (
     <section id="gallery" className="py-20 bg-background">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h1 className="text-[50px] sm:text-[100px] md:text-[120px] xl:text-[140px] font-extrabold tracking-tight text-gray-800 dark:text-gray-200">
+        <h1 className="text-[50px] sm:text-[100px] md:text-[120px] xl:text-[140px] font-extrabold tracking-tight text-gray-800 dark:text-gray-200">
           CONTACT
         </h1>
         <div
